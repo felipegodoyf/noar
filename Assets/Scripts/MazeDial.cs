ee9b03b655a5bbbc743241b93c20d1685d630598
felipegodoyf <felipegodoyf@outlook.com>
2020-09-27 23:51:10 -0300

    Maze

---
+++ Assets/Scripts/MazeDial.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MazeDial : MonoBehaviour
+{
+    [SerializeField] PlayerMazeInteraction playerMazeInteraction;
+    [SerializeField] UnityEvent successEvent;
+    Vector3 startPosition;
+
+    void Start ()
+    {
+        startPosition = transform.position;
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.name == "Maze")
+        {
+            transform.position = startPosition;
+            playerMazeInteraction.UnselectDial();
+            Debug.Log("Resetting dial");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "MazeGoal")
+        {
+            this.gameObject.SetActive(false);
+            successEvent.Invoke();
+        }
+    }
+
+    public void SelectDial()
+    {
+        playerMazeInteraction.SetDial(transform);
+    }
+
+    public void UnselectDial()
+    {
+        playerMazeInteraction.UnselectDial();
+    }
+}
---
+++ Assets/Scripts/PlayerMazeInteraction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMazeInteraction : MonoBehaviour
+{
+    [SerializeField] float maxDistance;
+    [SerializeField] LayerMask mazeLayerMask;
+    [SerializeField] float dialSpeed;
+    Transform dial;
+
+    void Update ()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, maxDistance, mazeLayerMask))
+        {
+            RaycastHit hit;
+            Physics.Raycast(transform.position, transform.forward, out hit, maxDistance, mazeLayerMask);
+
+            if (dial != null && hit.collider.gameObject.name != "Maze")
+            {
+                dial.position = Vector3.Lerp(dial.position, new Vector3(hit.point.x, hit.point.y, dial.position.z), dialSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    public void SetDial(Transform dial)
+    {
+        this.dial = dial;
+    }
+
+    public void UnselectDial()
+    {
+        this.dial = null;
+    }
+}
